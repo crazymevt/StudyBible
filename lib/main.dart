5b05e19159504058973345091269f3de6a85869e
Jessie Hughart <jhughart@gmail.com>
2026-06-18 22:24:39 -0400

    feat: Save and restore app window size/position using window_manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,43 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:window_manager/window_manager.dart';
 import 'ui/main_shell.dart';
 import 'app/shared_prefs.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  if (!kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux)) {
+    await windowManager.ensureInitialized();
+  }
+  
   final prefs = await SharedPreferences.getInstance();
+  
+  if (!kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux)) {
+    final width = prefs.getDouble('window_width') ?? 1200;
+    final height = prefs.getDouble('window_height') ?? 800;
+    final x = prefs.getDouble('window_x');
+    final y = prefs.getDouble('window_y');
+    
+    WindowOptions windowOptions = WindowOptions(
+      size: Size(width, height),
+      center: x == null || y == null,
+      backgroundColor: Colors.transparent,
+      skipTaskbar: false,
+      titleBarStyle: TitleBarStyle.normal,
+    );
+    
+    await windowManager.waitUntilReadyToShow(windowOptions, () async {
+      if (x != null && y != null) {
+        await windowManager.setPosition(Offset(x, y));
+      }
+      await windowManager.show();
+      await windowManager.focus();
+    });
+  }
   
   runApp(
     ProviderScope(
@@ -18,8 +49,52 @@ void main() async {
   );
 }
 
-class StudyBibleApp extends StatelessWidget {
+class StudyBibleApp extends ConsumerStatefulWidget {
   const StudyBibleApp({super.key});
+
+  @override
+  ConsumerState<StudyBibleApp> createState() => _StudyBibleAppState();
+}
+
+class _StudyBibleAppState extends ConsumerState<StudyBibleApp> with WindowListener {
+  @override
+  void initState() {
+    super.initState();
+    if (!kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux)) {
+      windowManager.addListener(this);
+    }
+  }
+
+  @override
+  void dispose() {
+    if (!kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux)) {
+      windowManager.removeListener(this);
+    }
+    super.dispose();
+  }
+  
+  void _saveWindowBounds() async {
+    if (kIsWeb || (!Platform.isMacOS && !Platform.isWindows && !Platform.isLinux)) return;
+    
+    final prefs = ref.read(sharedPreferencesProvider);
+    final size = await windowManager.getSize();
+    final position = await windowManager.getPosition();
+    
+    await prefs.setDouble('window_width', size.width);
+    await prefs.setDouble('window_height', size.height);
+    await prefs.setDouble('window_x', position.dx);
+    await prefs.setDouble('window_y', position.dy);
+  }
+
+  @override
+  void onWindowResized() {
+    _saveWindowBounds();
+  }
+
+  @override
+  void onWindowMoved() {
+    _saveWindowBounds();
+  }
 
   @override
   Widget build(BuildContext context) {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,22 +9,26 @@ import audio_service
 import audio_session
 import just_audio
 import package_info_plus
+import screen_retriever_macos
 import shared_preferences_foundation
 import sqflite_darwin
 import url_launcher_macos
 import video_player_avfoundation
 import wakelock_plus
 import webview_flutter_wkwebview
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
