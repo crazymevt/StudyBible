ffd29b8f936858e76658ed53a040aa9867de9ec5
Jessie Hughart <jhughart@gmail.com>
2026-07-15 06:00:54 -0400

    chore: release 26.7.15+1

--- lib/app/version.dart
+++ lib/app/version.dart
@@ -1,5 +1,5 @@
 // This file is the global version tracker.
 // It can be easily parsed or updated by build scripts.
 
-const String appVersion = '26.7.13';
+const String appVersion = '26.7.15';
 const int buildNumber = 1;
